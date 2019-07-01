81e851b89b73d3873dd0b23c1455f545db80466b
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-01 17:34:04 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(arryOfColor)
   
-  puts "R: #{arryOfColor[0]}, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  puts "R: #{arryOfColor[0]}, O: #{arryOfColor[1]}, Y: #{arryOfColor[2]} G: #{arryOfColor[3]}, B: #{arryOfColor[4]}, I: #{arryOfColor[5]}, V: #{arryOfColor[6]}"
   
 end
